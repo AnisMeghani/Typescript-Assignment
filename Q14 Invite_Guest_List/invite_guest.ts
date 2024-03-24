16c3e95a6d1465bed0e0788bb1bd0df5b35589be
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 01:01:13 +0500

    Add files via upload

---
+++ Q14 Invite_Guest_List/invite_guest.ts
@@ -0,0 +1,12 @@
+//21-02-2024   Ex.14
+//Guest List: If you could invite anyone, living or deceased, to dinner,
+//who would you invite? Make a list that includes at least three people you’d like to invite to dinner.
+//Then use your list to print a message to each person, inviting them to dinner.
+
+
+
+
+let guest_name :string[] = ['Abid', 'Hasnain', 'Raza']
+for(let i=0; i<guest_name.length; i++){
+    console.log('Dear Mr. ' + guest_name[i] + '\n\nIt is my pleasure to invite you in a birthday party of my son Muhammad Ayan Meghani.\n\nThankyou.\n')
+}
