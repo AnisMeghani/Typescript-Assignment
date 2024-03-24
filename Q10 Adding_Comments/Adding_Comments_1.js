50daab8d486fe04273529334062aad857d63cebc
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 00:55:44 +0500

    Add files via upload

---
+++ Q10 Adding_Comments/Adding_Comments_1.js
@@ -0,0 +1,5 @@
+"use strict";
+//This program is written by: Muhammad Anis Meghani
+//Current date: 21-02-2024
+//This program will write i have successfully added comments showing name and date
+console.log('i have successfully added two comments showing name and date');
---
+++ Q10 Adding_Comments/Adding_Comments_2.js
@@ -0,0 +1,11 @@
+"use strict";
+//Exercise 10
+//Adding Comments: Choose two of the programs you’ve written,
+//and add at least one comment to each.
+//If you don’t have anything specific to write because your programs are too simple at this point,
+//just add your name and the current date at the top of each program file.
+//Then write one sentence describing what the program does.
+//This program is written by: Muhammad Anis Meghani
+//Current date: 21-02-2024
+//This program will show digit
+console.log(5 + 5);
