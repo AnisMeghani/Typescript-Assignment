7d1657c2889070e6ea12dcdd1a111ffbb4d38cd5
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 00:56:55 +0500

    Add files via upload

---
+++ Q11 Names_Array/names_array.js
@@ -0,0 +1,5 @@
+"use strict";
+let friends_names = ['Abid', 'Hasnain', 'Raza', 'Arslan', 'Usman'];
+for (let i = 0; i < friends_names.length; i++) {
+    console.log(friends_names[i]);
+}
---
+++ Q11 Names_Array/names_array.ts
@@ -0,0 +1,10 @@
+//Exercise 11
+//Names: Store the names of a few of your friends in a array called names.
+//Print each person’s name by accessing each element in the list, one at a time.
+
+
+let friends_names :string[] = ['Abid','Hasnain','Raza','Arslan','Usman']
+for(let i=0; i<friends_names.length; i++){
+    console.log(friends_names[i])
+}
+
