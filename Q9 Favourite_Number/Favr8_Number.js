d766114905a53b0c1aceb2d912f5ee953501d462
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 00:52:59 +0500

    Add files via upload

---
+++ Q9 Favourite_Number/Favr8_Number.js
@@ -0,0 +1,7 @@
+"use strict";
+//Exercise 9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+//create a message that reveals your favorite number. Print that message.
+const Favourite_Number = 10;
+const message = 'Your favourite number is: ' + Favourite_Number;
+console.log(message);
---
+++ Q9 Favourite_Number/Favr8_Number.ts
@@ -0,0 +1,7 @@
+//Exercise 9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+//create a message that reveals your favorite number. Print that message.
+
+const Favourite_Number :number = 10 ;
+const message :string = 'Your favourite number is: ' + Favourite_Number ;
+console.log(message) ;
