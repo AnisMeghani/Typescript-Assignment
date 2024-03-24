08a37788168174541c7f011456514b99a2fc6aa0
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 01:10:14 +0500

    Add files via upload

---
+++ Q20 Empty_Array/Empty_Array.ts
@@ -0,0 +1,13 @@
+//03-03-2024 Exercise 20
+//Think of something you could store in a array.
+//For example, you could make a list of mountains, rivers, 
+//countries, cities, languages, or anything else you’d like.
+//Write a program that creates a list containing these items.
+
+let empty_array :string[] = [] ;
+empty_array.push('Karachi')
+empty_array.unshift('Pakistan')
+empty_array.push('National Language Urdu')
+
+//Write a program that creates a list containing these items.
+console.log(empty_array)
