5e4dc12f9509f92b0800d6a271567aca34d8c931
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 01:12:47 +0500

    Add files via upload

---
+++ Q22 Index_Error/index_Error.js
@@ -0,0 +1,11 @@
+"use strict";
+//03-03-2024 Exercise: 22
+//Intentional Error: If you haven’t received an array index error in one of your programs yet,
+//try to make one happen. Change an index in one of your programs to produce an index error.
+//Make sure you correct the error before closing the program.
+//creating array
+const Array1 = ['Ayan', 'Anabia', 'Mahnoor', 'Mysha'];
+//create error, index no.5 is not exist
+console.log(Array1[5]);
+//correction done
+console.log(Array1);
