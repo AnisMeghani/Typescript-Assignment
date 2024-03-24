2c8a759f4b74e561388a27ef9fa97b1eb41dd0ee
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 00:51:45 +0500

    Add files via upload

---
+++ Q7 & 8 Number8/Calculation_Num8.js
@@ -0,0 +1,12 @@
+"use strict";
+//Exercise 7 and 8
+//7.Number Eight: Write addition, subtraction, multiplication, and division operations
+//that each result in the number 8.
+//Be sure to enclose your operations in print statements to see the results.
+//8.You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(4 + 4); // Addition
+console.log(12 - 4); // Subtraction
+console.log(2 * 4); // Multiplication
+console.log(24 / 3); // Division
