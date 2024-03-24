b00642be391be8ce3123c8d362e2629818871f09
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 01:09:03 +0500

    Add files via upload

---
+++ Q19 Number_of_Invited_Guest/num_of_invited_guest.js
@@ -0,0 +1,10 @@
+"use strict";
+//03-03-2024  Exercise 19 from Ex.14
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+//print a message indicating the number of people you are inviting to dinner.
+let guest_name = ['Abid', 'Hasnain', 'Raza'];
+// for(let i=0; i<guest_name.length; i++){
+//     console.log('Dear Mr. ' + guest_name[i] + '\n\nIt is my pleasure to invite you in a birthday party of my son Muhammad Ayan Meghani.\n\nThankyou.\n')
+// }
+//print a message indicating the number of people you are inviting to dinner.
+console.log('Total invited friends are: ', guest_name.length);
