d598ecfe6059f6c24f9e40c15fc80a115ea54054
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 00:48:58 +0500

    Add files via upload

---
+++ Q5 Author_Quote_Variable/Variable_Quote.ts
@@ -0,0 +1,8 @@
+//Exercise 5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//Then compose your message and store it in a new variable called message.
+//Print your message.
+let QuotePerson :string = 'Venus Williams' ;
+let Quote :string = '"Just believe in yourself."' ;
+console.log(`${QuotePerson} once said, ${Quote}`) ;
+
