2b2ad44ee4de9023e6b6df83ebf9c1b7d01827f0
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-25 00:43:45 +0500

    Add files via upload

---
+++ Q1 & 2 Person_Name/Person_Name.ts
@@ -0,0 +1,19 @@
+// Exercise 1 and 2
+//1.Install Node.js, TypeScript and VS Code on your computer.
+
+//2.Personal Message: Store a person’s name in a variable
+//and print a message to that person. Your message should be simple,
+//such as, “Hello Eric, would you like to learn some Python today?”
+
+
+let personName :string = '';
+personName = prompt('Please enter your name?') || '';
+if(personName !== null && personName !== '')
+{alert(`Hello ${personName}, Please continue`)}
+else{alert('You have to fill your name !')}
+
+let Occupation : string = '';
+Occupation = prompt(`Please enter your Occupation?`) || '';
+if(Occupation !== null && Occupation !== '')
+{alert(`Ahh! ${Occupation}, Sounds Good`)}
+else{alert('You have to fill your Occupation!')}
